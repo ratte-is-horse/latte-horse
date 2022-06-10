92a19368df04889ca5c8c88e0fec4e5cfaf4c08b
Seohyun <rmsid1000@gmail.com>
2022-06-10 16:06:00 +0900

    기본 틀 및 링크 연결해둠

--- src/component/header.js
+++ src/component/header.js
@@ -1,20 +1,18 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 import styled from 'styled-components'
+import Signup from '../page/Signup';
 
 const Header = () => {
   return (
-    <Head>
-    <div>
-      
+    <Head>   
       <Title>
         <Link to="/" style={{textDecoration:"none", color:"black"}}>
         라떼는 말이야...
         </Link>
-      </Title>
-      
-      {/* <Login></Login> */}
-      </div>
+      </Title>   
+      <Login>로그인</Login>
+      <Signin>회원가입</Signin>
     </Head>
   )
 }
@@ -29,6 +27,16 @@ const Title = styled.h2`
  font-size:1.8rem;
   line-height:49px;
   text-align:center;
+`
+const Login = styled.div`
+position: fixed;
+top: 0;
+right: 120px;
+`
+const Signin = styled.div`
+position: fixed;
+top: 0;
+right: 40px;
 `
 
 export default Header
