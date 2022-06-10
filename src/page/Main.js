3c439c07ff443e9fec9c71230c7cdacb9ebdfb5f
ted-jv <wkejrdldls@gmail.com>
2022-06-10 20:02:17 +0900

    json-server

--- src/page/Main.js
+++ src/page/Main.js
@@ -1,7 +1,44 @@
 import React from "react";
+import axios from "axios";
 
 const Main = () => {
-  return <div>메인 페이지겠지</div>;
-};
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    e.persist();
 
+    let files = e.target.profile_files.files;
+    let formData = new FormData();
+    for (let i = 0; i < files.length; i++) {
+      formData.append("files", files[i]);
+    }
+
+    let dataSet = {
+      name: "Hong gil dong",
+      phone: "010-1234-1234",
+      birth: "2001-09-11",
+    };
+
+    formData.append("data", JSON.stringify(dataSet));
+
+    const postSurvey = await axios({
+      method: "POST",
+      url: "http://localhost:4000/posts",
+      mode: "cors",
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+      data: formData,
+    });
+
+    console.log(postSurvey);
+  };
+
+  return (
+    <form onSubmit={(e) => onSubmit(e)}>
+      <input type="file" name="profile_files" multiple="multiple" />
+
+      <button type="submit">제출</button>
+    </form>
+  );
+};
 export default Main;
