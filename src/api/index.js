bce6dc17229b70df24948303f431543a4e24f546
ted-jv <wkejrdldls@gmail.com>
2022-06-13 19:03:05 +0900

    로그인 setCookie까지 완성

--- src/api/index.js
+++ src/api/index.js
@@ -3,7 +3,7 @@ import axios from "axios";
 // Axios Instance를 생성:: 인스턴스를 이용하면 코드 중복을 최소화 할 수 있다.
 
 const api = axios.create({
-  baseURL: "http://localhost:4000",
+  baseURL: "http://52.79.226.242",
 });
 
 // Instance를 생성하고, 여러개의 요청 함수들을 하나의 객체에 넣어서 관리하는 방법도 있습니다!
@@ -11,6 +11,10 @@ const apis = {
   getPosts: () => api.get("/posts"),
   addUser: (newUser) => api.post("/posts", newUser),
   deletePost: (postId) => api.delete(`/posts/${postId}`),
+  postLogin: (userdata) => api.post("/api/user/login", userdata),
+  getLoiginCheck: () => api.get(""),
 };
+
+// addLogin: (UserData) => api.post("/posts", UserData)
 
 export default apis;
--- src/page/Login.js
+++ src/page/Login.js
@@ -1,30 +1,47 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { useRef } from "react";
-import apis from "../api";
+import { useDispatch } from "react-redux/es/exports";
+import { postUserJson } from "../redux/modules/user";
 
 const Login = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const id_ref = useRef(null);
   const password_ref = useRef(null);
 
-  const onLoginClick = async (event) => {
-    event.preventDefault();
-    // console.log(id_ref.current.value);
-    const data = await apis.addPost({
-      username: id_ref.current.value,
-      password: password_ref.current.value,
-    });
-    console.log(data);
+  const onLogin = (event) => {
+    event.preventDefault(); //1. !!아마 왼쪽꺼 필요없는 걸로 암.
+    console.log(id_ref.current.value);
+    dispatch(
+      postUserJson({
+        username: id_ref.current.value,
+        password: password_ref.current.value,
+      })
+    );
+    console.log(id_ref.current.value);
   };
 
   return (
     <>
       <div>로그인페이지</div>
-      <input type="text" ref={id_ref}></input>
-      <input type="password" ref={password_ref}></input>
-      <button onClick={onLoginClick}>로그인</button>
+      <form onSubmit={onLogin}>
+        <input type="text" ref={id_ref}></input>
+        <input type="password" ref={password_ref}></input>
+        <button type="submit">로그인</button>
+      </form>
     </>
   );
 };
+
+// const onLoginClick = async (event) => {
+//   event.preventDefault();
+//   // console.log(id_ref.current.value);
+//   const data = await apis.addPost({
+//     username: id_ref.current.value,
+//     password: password_ref.current.value,
+//   });
+//   console.log(data);
+// };
 
 export default Login;
